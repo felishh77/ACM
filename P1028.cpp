12d57b5b138ad111e48e22b451990f47133a75cb
felix07 <l.shuheng@stu.xjtu.edu.cn>
2025-09-24 22:41:55 +0800

    9.24

---
+++ P1028.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define inf 0x3f3f3f3f
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+}
--- pta/L1-009.cpp
+++ pta/L1-009.cpp
@@ -3,30 +3,50 @@
 
 using namespace std;
 
-bool cmp(pair<int, int> a, pair<int, int> b)
+ll gcd(ll a, ll b)
 {
-    return a.second > b.second;
+    return b == 0 ? a : gcd(b, a % b);
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
     int n;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> a;
-    cin >> n;
+    scanf("%d", &n);
+
+    ll sum_t = 0, sum_b = 1;  // 和的分子和分母
+
     for (int i = 0; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        a.push({x, y});
+        ll t, b;
+        scanf("%lld/%lld", &t, &b);
+
+        // 通分：sum = sum + t/b
+        ll new_b = sum_b * b / gcd(sum_b, b);                  // 新的分母
+        ll new_t = sum_t * (new_b / sum_b) + t * (new_b / b);  // 新的分子
+
+        sum_t = new_t;
+        sum_b = new_b;
+
+        // 约分
+        ll g = gcd(abs(sum_t), sum_b);
+        sum_t /= g;
+        sum_b /= g;
     }
-    sort(a.begin(), a.end());
-    for (int i = 0; i < n; i++) {
-        if (i) cout << ' ';
-        cout << a[i].second;
+
+    // 输出结果
+    if (sum_t == 0) {
+        printf("0\n");
+    } else {
+        ll integer_part = sum_t / sum_b;
+        ll remainder = sum_t % sum_b;
+
+        if (integer_part != 0 && remainder != 0) {
+            printf("%lld %lld/%lld\n", integer_part, abs(remainder), sum_b);
+        } else if (integer_part != 0) {
+            printf("%lld\n", integer_part);
+        } else {
+            printf("%lld/%lld\n", remainder, sum_b);
+        }
     }
-    cout << '\n';
 
     return 0;
 }
